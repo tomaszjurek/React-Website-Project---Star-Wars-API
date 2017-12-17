d5c97aed0c093a4a12b69947e218b40b8e2adf45
tomaszjurek <tomaszoprzadek@gmail.com>
2017-12-17 15:08:57 +0100

    Added colors to website

--- js/app.jsx
+++ js/app.jsx
@@ -13,8 +13,13 @@ class Page extends React.Component {
 
   render() {
     return (
-      <div className = "main-page">
-        <p>Stronka</p>
+      <div className = "main-body">
+        <header>{this.props.header}</header>
+        <nav className = "main-menu">{this.props.menu}</nav>
+        <div>
+          <main>{this.props.content}</main>
+        </div>
+        <footer>{this.props.footer}</footer>
       </div>
     )
   }
@@ -25,7 +30,30 @@ class App extends React.Component {
 
   render() {
     return (
-      <Page/>
+      <Page
+        header = {
+            <h1>Star Wars Galaxy of Information</h1>
+        }
+        content = {
+            <div>
+                <h1>Witaj na mojej stronie</h1>
+                <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+            </div>
+        }
+        menu = {
+            <ul>
+                <li><a href ="#">Main</a></li>
+                <li><a href ="#">Movies</a></li>
+                <li><a href ="#">Planets</a></li>
+                <li><a href ="#">People</a></li>
+                <li><a href ="#">Species</a></li>
+                <li><a href ="#">Starships</a></li>
+                <li><a href ="#">Vehicles</a></li>
+            </ul>
+        }
+        footer = {
+            <p>&copy; 2017 Tomasz O</p>
+        }/>
     );
   }
 
